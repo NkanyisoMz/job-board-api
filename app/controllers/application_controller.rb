8712cdc6de5ff6387e80bce05ffdf983ecdc5156
Nkanyiso Mzobe <104601010+NkanyisoMz@users.noreply.github.com>
2026-06-04 13:19:15 +0200

    Add centralized API error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::API
     include Pagy::Backend
+
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    rescue_from JWT::DecodeError, with: :invalid_token
+
+    private
+
+    def record_not_found(_exception)
+      render json: {
+        error: "Resource not found"
+      }, status: :not_found
+    end
+
+    def invalid_token
+      render json: {
+        error: "Invalid token"
+    }, status: :unauthorized
+    end
+
 end
